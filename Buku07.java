26e4c7da905102054f54ffeffcf4582f4256a302
Cakra Wangsa May Ahmad Widodo <Cakra Wangsa May Ahmad Widodo>
2024-02-19 14:07:24 +0700

    first commit

---
+++ Buku07.java
@@ -0,0 +1,39 @@
+public class Buku07 {
+
+    String judul, pengarang;
+    int halaman, stok, harga;
+
+    void tampilInformasi(){
+        System.out.println("Judul: " + judul);
+        System.out.println("Pengarang: " + pengarang);
+        System.out.println("Jumlah halaman: " + halaman);
+        System.out.println("Sisa stok: " + stok);
+        System.out.println("Harga: Rp " + harga);
+    }
+
+    void terjual(int jml){
+        if (stok > 0) {
+            stok -= jml;
+        }
+    }
+    
+    void restock(int jml){
+        stok += jml;
+    }
+
+    void gantiHarga(int hrg){
+        harga = hrg;
+    }
+
+    public Buku07(){
+        
+    }
+
+    public Buku07(String jud, String pg, int hal, int stok, int har){
+        judul = jud;
+        pengarang = pg;
+        halaman = hal;
+        this.stok = stok;
+        harga = har;
+    }
+}
---
+++ BukuMain07.java
@@ -0,0 +1,21 @@
+public class BukuMain07 {
+    public static void main(String[] args) {
+        Buku07 bk1 = new Buku07();
+        bk1.judul = "Today Ends Tomorrow Comes";
+        bk1.pengarang = "Denanda Pratiwi";
+        bk1.halaman = 198;
+        bk1.stok = 13;
+        bk1.harga = 71000;
+
+        bk1.tampilInformasi();
+        bk1.terjual(5);
+        bk1.gantiHarga(60000);
+        bk1.tampilInformasi();
+
+        Buku07 bk2 = new Buku07("Self Reward", "Maheera Ayesha", 160, 29, 59000);
+        bk2.terjual(11);
+        bk2.tampilInformasi();
+
+        Buku07 buku = new Buku07("Laskar Pelangi", "Andrea Hinata", 534, 19, 290000);
+    }
+}
